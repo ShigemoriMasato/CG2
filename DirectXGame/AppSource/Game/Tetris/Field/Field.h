5a849eb481214b01d24e7a80a770ceb0456701f6
Shigemori <k024g0027@g.neec.ac.jp>
2025-10-20 01:41:06 +0900

    テトリミノのオフセットだけ

--- DirectXGame/AppSource/Game/Tetris/Field/Field.h
+++ DirectXGame/AppSource/Game/Tetris/Field/Field.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <vector>
+
 class Field {
 public:
 
@@ -11,7 +13,8 @@ public:
 
 private:
 
-
+	std::vector<std::vector<int>> field_;
+	cosnt int width_ = 10;
+	const int height_ = 20;
 
 };
-
--- DirectXGame/AppSource/Game/Tetris/Mino/Tetrimino.h
+++ DirectXGame/AppSource/Game/Tetris/Mino/Tetrimino.h
@@ -31,4 +31,14 @@ private:
 	Type type_ = Type::None;
 	std::pair<int, int> pos_ = { 0, 0 };
 
+	std::unordered_map<Type, std::vector<std::pair<int, int>>> blockOffsets_ = {
+		{ Type::I, { {0, -1}, {0, 0}, {0, 1}, {0, 2} } },
+		{ Type::O, { {0, 0}, {1, 0}, {0, 1}, {1, 1} } },
+		{ Type::S, { {0, 0}, {1, 0}, {-1, 1}, {0, 1} } },
+		{ Type::Z, { {-1, 0}, {0, 0}, {0, 1}, {1, 1} } },
+		{ Type::J, { {-1, -1}, {-1, 0}, {0, 0}, {1, 0} } },
+		{ Type::L, { {1, -1}, {-1, 0}, {0, 0}, {1, 0} } },
+		{ Type::T, { {0, -1}, {-1, 0}, {0, 0}, {1, 0} } }
+	};
+
 };
